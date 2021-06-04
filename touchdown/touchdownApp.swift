a8c27001465578c7f06fcb36efdeeaa4eea3dfa3
irisida <20445648+irisida@users.noreply.github.com>
2021-06-04 20:22:48 +0100

    Initial Commit

---
+++ touchdown/touchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  touchdownApp.swift
+//  touchdown
+//
+//  Created by ed on 04/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct touchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
